a2d2362d842ea8f30a842d97d5c10363697ddebe
Maksim Pesetski <maksim.pesetski@gmail.com>
2019-11-21 16:45:14 -0500

    Working 16 points collision on Rectangle

--- app.js
+++ app.js
@@ -7,31 +7,66 @@ let tempScale;
 let rescale = true;
 
 //Start x & y:
+let noseX = 0;
+let noseY = 0;
+
+let leftShoulderX = 0;
+let leftShoulderY = 0;
+
+let rightShoulderX = 0;
+let rightShoulderY = 0;
+
+let leftElbowX = 0;
+let leftElbowY = 0;
+
+let rightElbowX = 0;
+let rightElbowY = 0;
+
 let leftWristX = 0;
 let leftWristY = 0;
 
 let rightWristX = 0;
 let rightWristY = 0;
 
+let leftHipX = 0;
+let leftHipY = 0;
+
+let rightHipX = 0;
+let rightHipY = 0;
+
+let rightKneeX = 0;
+let rightKneeY = 0;
+
+let leftKneeX = 0;
+let leftKneeY = 0;
+
+let leftAnkleX = 0;
+let leftAnkleY = 0;
+
+let rightAnkleX = 0;
+let rightAnkleY = 0;
 
 
-//setIntervalOne to grab and update the current scale for window;
-function setRescaleOne() {
-  setInterval(() => {
-    rescale = !rescale
-    tempScale = scale;
-  }, 6000);
-}
-//setIntervalTwo get invoked to update the current scale for window constantly;
-function setRescaleTwo() {
-  setInterval(() => {
-    rescale = !rescale
-    tempScale = scale;
-  }, 6000);
-}
+let keyPointsToCollide;
 
-setRescaleOne()
-setRescaleTwo()
+
+// //setIntervalOne to grab and update the current scale for window;
+// function setRescaleOne() {
+//   setInterval(() => {
+//     rescale = !rescale
+//     tempScale = scale;
+//   }, 6000);
+// }
+// //setIntervalTwo get invoked to update the current scale for window constantly;
+// function setRescaleTwo() {
+//   setInterval(() => {
+//     rescale = !rescale
+//     tempScale = scale;
+//   }, 6000);
+// }
+
+// setRescaleOne()
+// setRescaleTwo()
 
 
 //Getting acsess to camera
@@ -54,16 +89,119 @@ function setup() {
 function gotPoses(results) {
   poses = results;
   if (poses.length > 0) {
+
+    noseX = poses[0].pose.keypoints[0].position.x;
+    noseY = poses[0].pose.keypoints[0].position.y;
+
+    leftShoulderX = poses[0].pose.keypoints[5].position.x;
+    leftShoulderY = poses[0].pose.keypoints[5].position.y;
+
+    rightShoulderX = poses[0].pose.keypoints[6].position.x;
+    rightShoulderY = poses[0].pose.keypoints[6].position.y;
+
+    leftElbowX = poses[0].pose.keypoints[7].position.x;
+    leftElbowY = poses[0].pose.keypoints[7].position.y;
+
+    rightElbowX = poses[0].pose.keypoints[8].position.x;
+    rightElbowY = poses[0].pose.keypoints[8].position.y;
+
     leftWristX = poses[0].pose.keypoints[9].position.x;
     leftWristY = poses[0].pose.keypoints[9].position.y;
 
     rightWristX = poses[0].pose.keypoints[10].position.x;
     rightWristY = poses[0].pose.keypoints[10].position.y;
+
+    leftHipX = poses[0].pose.keypoints[11].position.x;
+    leftHipY = poses[0].pose.keypoints[11].position.y;
+
+    rightHipX = poses[0].pose.keypoints[12].position.x;
+    rightHipY = poses[0].pose.keypoints[12].position.y;
+
+    rightKneeX = poses[0].pose.keypoints[13].position.x;
+    rightKneeY = poses[0].pose.keypoints[13].position.y;
+
+    leftKneeX = poses[0].pose.keypoints[14].position.x;
+    leftKneeY = poses[0].pose.keypoints[14].position.y;
+
+    leftAnkleX = poses[0].pose.keypoints[15].position.x;
+    leftAnkleY = poses[0].pose.keypoints[15].position.y;
+
+    rightAnkleX = poses[0].pose.keypoints[16].position.x;
+    rightAnkleY = poses[0].pose.keypoints[16].position.y;
+
+    keyPointsToCollide = {
+      nose: {
+        x: noseX,
+        y: noseY
+      },
+      leftShoulder: {
+        x: leftShoulderX,
+        y: leftShoulderY
+      },
+      rightShoulder: {
+        x: rightShoulderX,
+        y: rightShoulderY
+      },
+      leftElbow: {
+        x: leftElbowX,
+        y: leftElbowY
+      },
+      rightElbow: {
+        x: rightElbowX,
+        y: rightElbowY
+      },
+      leftWrist: {
+        x: leftWristX,
+        y: leftWristY
+      },
+      rightWrist: {
+        x: rightWristX,
+        y: rightWristY
+      },
+      leftHip: {
+        x: leftHipX,
+        y: leftHipY
+      },
+      rightHip: {
+        x: rightHipX,
+        y: rightHipY
+      },
+      rightKnee: {
+        x: rightKneeX,
+        y: rightKneeY
+      },
+      leftKnee: {
+        x: leftKneeX,
+        y: leftKneeY
+      },
+      leftAnkle: {
+        x: leftAnkleX,
+        y: leftAnkleY
+      },
+      rightAnkle: {
+        x: rightAnkleX,
+        y: rightAnkleY
+      }
+    }
   }
 }
 
 // Test collision
-let hit = false;
+let hit = {
+  nose: false,
+  leftShoulder: false,
+  rightShoulder: false,
+  leftElbow: false,
+  rightElbow: false,
+  leftWrist: false,
+  rightWrist: false,
+  leftHip: false,
+  rightHip: false,
+  rightKnee: false,
+  leftKnee: false,
+  leftAnkle: false,
+  rightAnkle: false
+}
 
 function modelReady() {
   select('#status').html('Model Loaded');
@@ -75,16 +213,23 @@ function draw() {
   //You acn add filters by insertinf filter()
 
   // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
-  drawSkeleton();
+  // drawKeypoints();
+  // drawSkeleton();
+
+  rect(200, 50, 800, 1000);
+  for (let pointName in keyPointsToCollide) {
+    ellipse(keyPointsToCollide[pointName].x, keyPointsToCollide[pointName].y, 20, 20);
+
+    // hit[pointName] = collideRectCircle(200, 50, 800, 1000, keyPointsToCollide[pointName].x, keyPointsToCollide[pointName].y, 100)
+
+    hit = { ...hit, [pointName]: collideRectCircle(200, 50, 800, 1000, keyPointsToCollide[pointName].x, keyPointsToCollide[pointName].y, 100) }
+
+    // print("colliding? " + hit);
+
+    console.log('HIT OBJECT>>>>', hit)
+  }
 
 
-  rect(200, 200, 100, 150);
-
-  ellipse(rightWristX, rightWristY, 100, 100);
-
-  hit = collideRectCircle(200, 200, 100, 150, rightWristX, rightWristY, 100);
-  print("colliding? " + hit);
 
   // if (rescale) {
   //   drawWindowPoints();

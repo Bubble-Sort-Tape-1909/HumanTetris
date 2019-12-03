5be4bc4b0dbdedea09e39e43566f91e8ed995e57
Sam Miller <sammillerks@gmail.com>
2019-12-03 15:02:59 -0500

    final commit for working singleplayer

--- website/client/components/SinglePlayerPage/SinglePlayerPage.js
+++ website/client/components/SinglePlayerPage/SinglePlayerPage.js
@@ -2,25 +2,6 @@ import React from 'react'
 const load = require('load-script')
 
 
-// const SinglePlayerPage = () => {
-//   return <div id="game" />
-// }
-
-// const SinglePlayerPage = React.createClass({
-//   render: function() {
-//     return (
-//       <div>
-//         <meta charSet="utf-8" />
-//         <meta name="viewport" content="width=device-width initial-scale=1.0" />
-//         <title>Human Tetris</title>
-//         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-//         <div id="app" />
-//       </div>
-//     );
-//   }
-// });
-
-
 export default class SinglePlayerPage extends React.Component {
   constructor(props) {
     super(props);
@@ -37,16 +18,6 @@ export default class SinglePlayerPage extends React.Component {
   })
 
   render() {
-    // const scriptTag = load("app.js", function (err, script) {
-    //   if (err) {
-    //     // print useful message
-    //   }
-    //   else {
-    //     console.log(script.src);// Prints 'foo'.js'
-    //     // use script
-    //     // note that in IE8 and below loading error wouldn't be reported
-    //   }
-    // })
     return (
       <div>
         {/* <button onClick = {this.forceUpdate()} >Play Again</button> */}

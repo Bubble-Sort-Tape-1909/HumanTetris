81551390a56a9ac04dee89b7fec2426d57dafe13
Sam Miller <sammillerks@gmail.com>
2019-12-05 12:32:35 -0500

    leaderboard working

--- website/server/firestore/databaseFunctions.js
+++ website/server/firestore/databaseFunctions.js
@@ -8,7 +8,7 @@ async function addUser(user) {
     let userDoesNotExist = false
   let collection = db.collection('TestUsers')
   await collection.where('Email', '==', `${user.Email}`).get().then((snapshot) => {
-      console.log(snapshot.docs)
+      // console.log(snapshot.docs)
       if(snapshot.docs.length === 0){
           userDoesNotExist = true
       }
@@ -20,52 +20,69 @@ async function addUser(user) {
     .set(user)
     .then(console.log(`${user.Email} added to the database.`))
   }
-  
-//   console.log('exists', !!collection.doc(`${user.Email}`))
-//   console.log('doesnt exist', !collection.doc(`${user.Email}`))
-//   if (!collection.doc(`${user.Email}`)){
-//       collection
-//       .doc(`${user.Email}`)
-//       .set(user)
-//       .then(console.log(`${user.Email} added to the database.`))
-//   }
 }
 
-//find top ten scores in db
+function deleteQueryBatch(db, query, batchSize, resolve, reject) {
+  query.get()
+    .then((snapshot) => {
+      // When there are no documents left, we are done
+      if (snapshot.size == 0) {
+        return 0;
+      }
+
+      // Delete documents in a batch
+      let batch = db.batch();
+      snapshot.docs.forEach((doc) => {
+        batch.delete(doc.ref);
+      });
+
+      return batch.commit().then(() => {
+        return snapshot.size;
+      });
+    }).then((numDeleted) => {
+      if (numDeleted === 0) {
+        resolve();
+        return;
+      }
+
+      // Recurse on the next process tick, to avoid
+      // exploding the stack.
+      process.nextTick(() => {
+        deleteQueryBatch(db, query, batchSize, resolve, reject);
+      });
+    })
+    .catch(reject);
+}
+
+//find top ten scores in collection HighScoresLeaderboard
+//returns the leaderboard(top ten scores) in a sorted array, so lowest score is last index of the array
 async function getTopTenHighScores() {
   let highscoresArray = [];
-  let query = await db.collection('TestUsers').orderBy('Scores', 'asc').get().then((snapshot) => {
+  let query = await db.collection('HighScoresLeaderboard').orderBy('Score', 'desc').limit(10).get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
-      highscoresArray.push(doc)
-      console.log(doc.data())
+      highscoresArray.push(doc.data())
+      // console.log(doc.data())
     })
   })
-  // console.log(highscoresArray)
-  // return query
+  console.log(highscoresArray)
+  return highscoresArray
 }
- 
 
-async function getUser(email) {
-    // const collection = await db.collection('TestUsers')
-    
-    // const targetUser = await collection.where('email', '==', email).get()
-    // console.log(targetUser)
 
-    let userRef = db.collection('TestUsers').doc(email);
-    cityRef.update({Scores: [44, 13, 150]})
-    console.log('our data?', await cityRef.get())
-    let getDoc = cityRef.get()
-  .then(doc => {
-    if (!doc.exists) {
-      console.log('No such document!');
-    } else {
-        
-    //   console.log('Document data:', doc.data());
-    }
-  })
-  .catch(err => {
-    console.log('Error getting document', err);
-  });
+//we need to check if it's in the top ten highest scores in collection HighScoresLeaderboard
+//if it is, delete the 10th score and insert it into the collection
+async function addToHighScores(score, displayName) {
+  const newHighScore = {
+    DisplayName: displayName,
+    Score: score
+  }
+  const collection = db.collection('HighScoresLeaderboard')
+
+  const highScoresArray = await getTopTenHighScores()
+  if (score > highScoresArray.slice(-1)[0].Score && score > 1000) {
+    collection.doc().set(newHighScore)
+  }
+
 }
 
 
@@ -95,4 +112,4 @@ async function addScore(email, newScore) {
 }
 
 
-module.exports = {addUser, getTopTenHighScores, addScore, getUser}
+module.exports = {addUser, getTopTenHighScores, addScore, addToHighScores}
